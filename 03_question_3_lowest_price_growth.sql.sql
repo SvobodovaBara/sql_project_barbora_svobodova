27d3476544243ddad609831049603acb238df937
SvobodovaBara <stavby.hlavac@seznam.cz>
2025-11-21 16:28:56 +0100

    03_question_3_lowest_price_growth.sql.sql

--- 03_question_3_lowest_price_growth.sql.sql
+++ 03_question_3_lowest_price_growth.sql.sql
@@ -1,22 +1,11 @@
-WITH prices_per_year AS (
-    SELECT
-        EXTRACT(YEAR FROM cp.date_from)::INT AS year,
-        pc.name AS category_name,
-        AVG(cp.value) AS avg_price
-    FROM czechia_price cp
-    LEFT JOIN czechia_price_category pc
-        ON cp.category_code = pc.code
-    WHERE cp.value IS NOT NULL
-    GROUP BY EXTRACT(YEAR FROM cp.date_from)::INT, pc.name
-),
-price_growth AS (
+WITH price_growth AS (
     SELECT
         category_name,
         year,
         avg_price,
         (avg_price - LAG(avg_price) OVER (PARTITION BY category_name ORDER BY year))
-        / LAG(avg_price) OVER (PARTITION BY category_name ORDER BY year) * 100 AS percent_growth
-    FROM prices_per_year
+        / NULLIF(LAG(avg_price) OVER (PARTITION BY category_name ORDER BY year), 0) * 100 AS percent_growth
+    FROM data_academy_content.t_barbora_svobodova_project_sql_primary_final
 )
 SELECT category_name,
        ROUND(AVG(percent_growth)::numeric, 2) AS avg_annual_percent_growth
